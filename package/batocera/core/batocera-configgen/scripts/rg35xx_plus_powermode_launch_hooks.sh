2dc4cfb99b5bb57df1c872deaa488ff1d87d6a27
Philippe Simons <simons.philippe@gmail.com>
2024-03-16 11:32:00 +0100

    set some system default powermode

---
+++ package/batocera/core/batocera-configgen/scripts/rg35xx_plus_powermode_launch_hooks.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+GPU_CTRL="/sys/kernel/debug/sunxi_gpu/write"
+
+if ! [ -e /sys/kernel/debug/sunxi_gpu/write ]; then
+	exit 0
+fi
+
+
+set_gpu_settings() {
+	local FREQUENCY=$1
+    local VOLTAGE=$2
+
+	echo "frequency:1"          > $GPU_CTRL
+	echo "voltage:$VOLTAGE"     > $GPU_CTRL
+	echo "frequency:$FREQUENCY" > $GPU_CTRL
+}
+
+handle_powermode() {
+	local POWERMODE_NAME=$1
+		case "$POWERMODE_NAME" in
+			"highperformance")
+				set_gpu_settings "648" "960"
+				;;
+			"balanced")
+				set_gpu_settings "552" "900"
+				;;
+			"powersaver")
+				set_gpu_settings "420" "900"
+				;;
+			*)
+		esac				
+}
+
+# Check for events
+EVENT=$1
+SYSTEM_NAME=$2
+
+# Exit if the event is neither gameStart nor gameStop
+if [ "$EVENT" != "gameStart" ] && [ "$EVENT" != "gameStop" ]; then
+    exit 0
+fi
+
+# Handle gameStop event
+if [ "$EVENT" = "gameStop" ]; then
+	set_gpu_settings "420" "900"
+	exit 0
+fi
+
+# Check for user set system specific setting
+if [ -n "${SYSTEM_NAME}" ]; then
+    POWER_MODE_SETTING="${SYSTEM_NAME}.powermode"
+    POWER_MODE="$(/usr/bin/batocera-settings-get-master "${POWER_MODE_SETTING}")"
+fi
+
+# If no user set system specific setting check for user set global setting
+if [ -z "${POWER_MODE}" ]; then
+    POWER_MODE="$(/usr/bin/batocera-settings-get-master global.powermode)"
+fi
+
+# If no value is found ensure governor is system default before exiting
+if [ -z "${POWER_MODE}" ]; then
+	set_gpu_settings "420" "900"
+	exit 0
+fi
+
+# select powermode
+if ! [ -z "${POWER_MODE}" ]; then
+	handle_powermode "${POWER_MODE}"
+fi
