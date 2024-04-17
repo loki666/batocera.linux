90a0277551eeb8033849711843cbfe2bfad044e5
Philippe Simons <simons.philippe@gmail.com>
2024-04-17 20:43:35 +0200

    use knulli-cfw package for mali

--- package/batocera/gpu/mali-g31-fbdev/mali-g31-fbdev.mk
+++ package/batocera/gpu/mali-g31-fbdev/mali-g31-fbdev.mk
@@ -1,52 +1,43 @@
 ################################################################################
 #
-# mali mp400 gbm
+# mali g31 fbdev
 #
 ################################################################################
-# Version.: Commits on Feb 24, 2022
+# Version.: Commits on Apr 11, 2024
+MALI_G31_FBDEV_VERSION = f91ad32d45f09b9ea437bd19b8099b30400bb85f
+MALI_G31_FBDEV_SITE = $(call github,knulli-cfw,t507_gpu_drivers,$(MALI_G31_FBDEV_VERSION))
+MALI_G31_FBDEV_LICENSE = Proprietary
+MALI_G31_FBDEV_LICENSE_FILES = END_USER_LICENCE_AGREEMENT.txt
+
 MALI_G31_FBDEV_INSTALL_STAGING = YES
 MALI_G31_FBDEV_PROVIDES = libegl libgles libmali
 
 define MALI_G31_FBDEV_INSTALL_STAGING_CMDS
 	mkdir -p $(STAGING_DIR)/usr/lib/pkgconfig
 
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/gpu/mali-g31-fbdev/libmali.so \
-		$(STAGING_DIR)/usr/lib/libmali.so
+	cp -rf $(@D)/include/* $(STAGING_DIR)/usr/include/
+	cp -rf $(@D)/fbdev/include/EGL/* $(STAGING_DIR)/usr/include/EGL/
 
-	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libMali.so)
-	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libmali.so.0)
-	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libmali.so.1)
-	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libEGL.so)
-	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libEGL.so.1)
-	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libGLESv1_CM.so)
-	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libGLESv1_CM.so.1)
-	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libGLESv2.so)
-	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libGLESv2.so.2)
+	cp -rf $(@D)/fbdev/mali-g31/aarch64-linux-gnu-7.4.1/lib64/libmali.so.0.20.0 $(STAGING_DIR)/usr/lib/
 
-        cp -rf $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/gpu/mali-g31-fbdev/include/* $(STAGING_DIR)/usr/include/
+	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so.0.20.0 libmali.so)
+	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so.0.20.0 libmali.so.0)
+	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so.0.20.0 libEGL.so)
+	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so.0.20.0 libEGL.so.1)
+	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so.0.20.0 libGLESv1_CM.so)
+	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so.0.20.0 libGLESv1_CM.so.1)
+	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so.0.20.0 libGLESv2.so)
+	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so.0.20.0 libGLESv2.so.2)
 
-        $(INSTALL) -D -m 0644  $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/gpu/mali-g31-fbdev/egl.pc \
-                $(STAGING_DIR)/usr/lib/pkgconfig/egl.pc
-        $(INSTALL) -D -m 0644  $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/gpu/mali-g31-fbdev/glesv2.pc \
-                $(STAGING_DIR)/usr/lib/pkgconfig/glesv2.pc
-
+	$(INSTALL) -D -m 0644  $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/gpu/mali-g31-fbdev/egl.pc \
+			$(STAGING_DIR)/usr/lib/pkgconfig/egl.pc
+	$(INSTALL) -D -m 0644  $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/gpu/mali-g31-fbdev/glesv2.pc \
+			$(STAGING_DIR)/usr/lib/pkgconfig/glesv2.pc
 endef
 
 define MALI_G31_FBDEV_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib
-	
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/gpu/mali-g31-fbdev/libmali.so \
-		$(TARGET_DIR)/usr/lib/libmali.so
-
-	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libMali.so)
-	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libmali.so.0)
-	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libmali.so.1)
-	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libEGL.so)
-	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libEGL.so.1)
-	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libGLESv1_CM.so)
-	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libGLESv1_CM.so.1)
-	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libGLESv2.so)
-	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libGLESv2.so.2)
+	cp -rf $(@D)/fbdev/mali-g31/aarch64-linux-gnu-7.4.1/lib64/* $(TARGET_DIR)/usr/lib/
 endef
 
 $(eval $(generic-package))
